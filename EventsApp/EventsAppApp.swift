e720d04e889f30fe08446fbd84f918863ef5fe2c
Pall Arnold-Barna <pall.arnold@codespring.ro>
2023-03-10 17:52:21 +0200

    Initial Commit

---
+++ EventsApp/EventsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventsAppApp.swift
+//  EventsApp
+//
+//  Created by Páll Arnold-Barna on 10.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct EventsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
