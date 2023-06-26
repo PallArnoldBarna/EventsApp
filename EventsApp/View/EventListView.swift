f1b2e5b7914623ffcc2d87887a020eb7c26f4b57
Pall Arnold-Barna <pall.arnold@codespring.ro>
2023-06-26 15:20:04 +0300

    Event filtering bases on the date and the type added, some refactoring

--- EventsApp/View/EventListView.swift
+++ EventsApp/View/EventListView.swift
@@ -8,45 +8,159 @@
 import SwiftUI
 
 struct EventListView: View {
-    let navbarTitle: String
+    let eventType: String
     @State var tabIndex = 1
     @EnvironmentObject var getEventsViewModel: GetEventsViewModel
-       
-       var body: some View {
-           VStack{
-               CustomTopTabBar(tabIndex: $tabIndex)
-               if tabIndex == 0 {
-                   Spacer()
-                   Text("First")
-               }
-               else if tabIndex == 1 {
-                   List(getEventsViewModel.events, id: \.name) { event in
-                       ZStack {
-                           NavigationLink(destination: EventDetailView(event: event)) {
-                               EmptyView()
-                           }
-                           .opacity(0.0)
-                           .buttonStyle(PlainButtonStyle())
-                           
-                           EventRowView(event: event)
-                       }
-                   }
-                   .listStyle(.inset)
-               }
-               else if tabIndex == 2 {
-                   Spacer()
-                   Text("Third")
-               }
-               Spacer()
-           }
-           .frame(width: UIScreen.main.bounds.width - 24, alignment: .center)
-           .padding(.horizontal, 12)
-           .navigationBarTitle(navbarTitle, displayMode: .inline)
-       }
+    
+    var body: some View {
+        VStack{
+            CustomTopTabBar(tabIndex: $tabIndex)
+            if tabIndex == 0 {
+                if eventType == "All events" {
+                    List(filteredEventsPrevious, id: \.name) { event in
+                        ZStack {
+                            NavigationLink(destination: EventDetailView(event: event)) {
+                                EmptyView()
+                            }
+                            .opacity(0.0)
+                            .buttonStyle(PlainButtonStyle())
+                            
+                            EventRowView(event: event)
+                        }
+                    }
+                    .listStyle(.inset)
+                } else if eventType != "All events" {
+                    List(filteredEventsPreviousByType, id: \.name) { event in
+                        ZStack {
+                            NavigationLink(destination: EventDetailView(event: event)) {
+                                EmptyView()
+                            }
+                            .opacity(0.0)
+                            .buttonStyle(PlainButtonStyle())
+                            
+                            EventRowView(event: event)
+                        }
+                    }
+                    .listStyle(.inset)
+                }
+            }
+            else if tabIndex == 1 {
+                if eventType == "All events" {
+                    List(filteredEventsCurrent, id: \.name) { event in
+                        ZStack {
+                            NavigationLink(destination: EventDetailView(event: event)) {
+                                EmptyView()
+                            }
+                            .opacity(0.0)
+                            .buttonStyle(PlainButtonStyle())
+                            
+                            EventRowView(event: event)
+                        }
+                    }
+                    .listStyle(.inset)
+                } else if eventType != "All events" {
+                    List(filteredEventsCurrentByType, id: \.name) { event in
+                        ZStack {
+                            NavigationLink(destination: EventDetailView(event: event)) {
+                                EmptyView()
+                            }
+                            .opacity(0.0)
+                            .buttonStyle(PlainButtonStyle())
+                            
+                            EventRowView(event: event)
+                        }
+                    }
+                    .listStyle(.inset)
+                }
+            }
+            else if tabIndex == 2 {
+                if eventType == "All events" {
+                    List(filteredEventsUpcoming, id: \.name) { event in
+                        ZStack {
+                            NavigationLink(destination: EventDetailView(event: event)) {
+                                EmptyView()
+                            }
+                            .opacity(0.0)
+                            .buttonStyle(PlainButtonStyle())
+                            
+                            EventRowView(event: event)
+                        }
+                    }
+                    .listStyle(.inset)
+                } else if eventType != "All events" {
+                    List(filteredEventsUpcomingByType, id: \.name) { event in
+                        ZStack {
+                            NavigationLink(destination: EventDetailView(event: event)) {
+                                EmptyView()
+                            }
+                            .opacity(0.0)
+                            .buttonStyle(PlainButtonStyle())
+                            
+                            EventRowView(event: event)
+                        }
+                    }
+                    .listStyle(.inset)
+                }
+            }
+            Spacer()
+        }
+        .frame(width: UIScreen.main.bounds.width - 24, alignment: .center)
+        .padding(.horizontal, 12)
+        .navigationBarTitle(eventType, displayMode: .inline)
+    }
+    
+    var filteredEventsPrevious: [Event] {
+        let currentDate = Date()
+        
+        return getEventsViewModel.events.filter {
+            let itemDay = Calendar.current.component(.day, from: $0.endDate)
+            let currentDay = Calendar.current.component(.day, from: currentDate)
+            return itemDay < currentDay
+        }
+    }
+    
+    var filteredEventsPreviousByType: [Event] {
+        return filteredEventsPrevious.filter {
+            $0.category == eventType
+        }
+    }
+    
+    var filteredEventsCurrent: [Event] {
+        let currentDate = Date()
+        
+        return getEventsViewModel.events.filter {
+            let itemDay = Calendar.current.component(.day, from: $0.startDate)
+            let currentDay = Calendar.current.component(.day, from: currentDate)
+            return itemDay == currentDay
+        }
+    }
+    
+    var filteredEventsCurrentByType: [Event] {
+        return filteredEventsCurrent.filter {
+            $0.category == eventType
+        }
+    }
+    
+    var filteredEventsUpcoming: [Event] {
+        let currentDate = Date()
+        
+        return getEventsViewModel.events.filter {
+            let itemDay = Calendar.current.component(.day, from: $0.startDate)
+            let currentDay = Calendar.current.component(.day, from: currentDate)
+            return itemDay > currentDay
+        }
+    }
+    
+    var filteredEventsUpcomingByType: [Event] {
+        return filteredEventsUpcoming.filter {
+            $0.category == eventType
+        }
+    }
+    
 }
 
 struct EventListView_Previews: PreviewProvider {
     static var previews: some View {
-        EventListView(navbarTitle: "All events")
+        EventListView(eventType: "All events")
     }
 }
--- EventsApp/View/HomePageView.swift
+++ EventsApp/View/HomePageView.swift
@@ -52,29 +52,29 @@ struct HomePageView: View {
                     Spacer()
                     VStack {
                         HStack {
-                            NavigationLink(destination: EventListView(navbarTitle: "All events")) {
+                            NavigationLink(destination: EventListView(eventType: "All events")) {
                                 CardView(cardWidth: CGFloat(BigCardSizes.cardSize), cardHeight: CGFloat(BigCardSizes.cardSize), cardColor: .red, vStackSpacing: 10, cardImageString: "globe_icon", cardImageWidth: CGFloat(BigCardSizes.imageSize), cardImageHeight: CGFloat(BigCardSizes.imageSize), cardImageColor: .white, cardText: "All events", cardTextColor: .white)
                             }
                             
                             VStack() {
-                                NavigationLink(destination: EventListView(navbarTitle: "Parties")) {
+                                NavigationLink(destination: EventListView(eventType: "Party")) {
                                     CardView(cardWidth: CGFloat(SmallCardSizes.cardSize), cardHeight: CGFloat(SmallCardSizes.cardSize), cardColor: .blue, vStackSpacing: 10, cardImageString: "party_icon", cardImageWidth: CGFloat(SmallCardSizes.imageSize), cardImageHeight: CGFloat(SmallCardSizes.imageSize), cardImageColor: .white, cardText: "Party", cardTextColor: .white)
                                 }
-                                NavigationLink(destination: EventListView(navbarTitle: "Festivals")) {
+                                NavigationLink(destination: EventListView(eventType: "Festival")) {
                                     CardView(cardWidth: CGFloat(SmallCardSizes.cardSize), cardHeight: CGFloat(SmallCardSizes.cardSize), cardColor: .yellow, vStackSpacing: 10, cardImageString: "festival_icon", cardImageWidth: CGFloat(SmallCardSizes.imageSize), cardImageHeight: CGFloat(SmallCardSizes.imageSize), cardImageColor: .white, cardText: "Festival", cardTextColor: .white)
                                 }
                             }
                         }
                         HStack() {
-                            NavigationLink(destination: EventListView(navbarTitle: "Conferences")) {
+                            NavigationLink(destination: EventListView(eventType: "Conference")) {
                                 CardView(cardWidth: CGFloat(SmallCardSizes.cardSize), cardHeight: CGFloat(SmallCardSizes.cardSize), cardColor: .orange, vStackSpacing: 10, cardImageString: "conference_icon", cardImageWidth: CGFloat(SmallCardSizes.imageSize), cardImageHeight: CGFloat(SmallCardSizes.imageSize), cardImageColor: .white, cardText: "Conference", cardTextColor: .white)
                             }
                             
-                            NavigationLink(destination: EventListView(navbarTitle: "Sport events")) {
+                            NavigationLink(destination: EventListView(eventType: "Sport")) {
                                 CardView(cardWidth: CGFloat(SmallCardSizes.cardSize), cardHeight: CGFloat(SmallCardSizes.cardSize), cardColor: .green, vStackSpacing: 10, cardImageString: "sport_icon", cardImageWidth: CGFloat(SmallCardSizes.imageSize), cardImageHeight: CGFloat(SmallCardSizes.imageSize), cardImageColor: .white, cardText: "Sport", cardTextColor: .white)
                             }
                             
-                            NavigationLink(destination: EventListView(navbarTitle: "Cultural")) {
+                            NavigationLink(destination: EventListView(eventType: "Cultural")) {
                                 CardView(cardWidth: CGFloat(SmallCardSizes.cardSize), cardHeight: CGFloat(SmallCardSizes.cardSize), cardColor: .purple, vStackSpacing: 10, cardImageString: "cultural_icon", cardImageWidth: CGFloat(SmallCardSizes.imageSize), cardImageHeight: CGFloat(SmallCardSizes.imageSize), cardImageColor: .white, cardText: "Cultural", cardTextColor: .white)
                             }
                         }
